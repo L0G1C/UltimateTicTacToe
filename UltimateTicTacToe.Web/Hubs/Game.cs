ce9b17b03bfc1a994cb6689054f3a865c283b5ce
Leo Jerez <Leo.Jerez@inmar.com>
2018-02-21 17:42:49 -0500

    Trying to do a hello world here.

--- UltimateTicTacToe.Web/Hubs/Game.cs
+++ UltimateTicTacToe.Web/Hubs/Game.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace UltimateTicTacToe.Web.Hubs
 {
-    public class Game
+    public class Game : Hub
     {
     }
 }
---
+++ UltimateTicTacToe.Web/wwwroot/js/game.ts
@@ -0,0 +1,8 @@
+﻿let connection = new signalR.HubConnection('/game');
+
+connection.on('send', data => {
+    console.log(data);
+});
+
+connection.start()
+    .then(() => connection.invoke('send', 'Hello World'));
